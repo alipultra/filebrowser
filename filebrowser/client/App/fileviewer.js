120e2a247808206a4758fb55da8cfdd840d95975
alip putra <alipputra@alips-MacBook-Pro.local>
2016-11-28 16:13:54 +0700

    word 2007 extension

--- filebrowser/client/App/fileviewer.js
+++ filebrowser/client/App/fileviewer.js
@@ -39,7 +39,7 @@ soyut.Services.getInstance().getService("browserServer").getDocServerUrl({}, fun
         var html = "<img src=\"" + url + "\" height=\"100%\" width=\"100%\">";
         $('.content-data').html(html);
     }
-    else if (type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+    else if (type == "application/msword") {
 
         soyut.Services.getInstance().getService("browserServer").generateDocKey({}, function (err, data) {
             initDocEditor(data.key, data.vkey);
@@ -75,7 +75,8 @@ soyut.Services.getInstance().getService("browserServer").getDocServerUrl({}, fun
         });
 
     }
-    else if (type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") {
+    else if (type == "application/vnd.ms-excel") {
+
         soyut.Services.getInstance().getService("browserServer").generateSheetKey({}, function (err, data) {
             initSheetEditor(data.key, data.vkey);
             function initSheetEditor(docKey, docVkey) {
@@ -109,10 +110,113 @@ soyut.Services.getInstance().getService("browserServer").getDocServerUrl({}, fun
         });
 
     }
-    else if (type == "application/vnd.openxmlformats-officedocument.presentationml.presentation") {
+    else if (type == "application/vnd.ms-powerpoint") {
         soyut.Services.getInstance().getService("browserServer").generatePresentationKey({}, function (err, data) {
             initPresentationEditor(data.key, data.vkey);
             function initPresentationEditor(docKey, docVkey) {
+                var docEditor = new DocsAPI.DocEditor(frameEditorId[1],
+                    {
+                        width: "800px",
+                        height: "800px",
+                        documentType: "presentation",
+                        document: {
+                            title: name,
+                            url: url,
+                            key: docKey,
+                            vkey: docVkey,
+                            permissions: {
+                                download: false,
+                                print: false,
+                            }
+                        },
+                        editorConfig: {
+                            lang: "en",
+                            location: documentServerUrl + "/web-apps/",
+                            customization: {
+                                about: false,
+                                logo: {
+                                    image: 'https://' + browserService.origin + '/img/soyut.png'
+                                }
+                            }
+                        }
+                    });
+            }
+        });
+    }
+    else if (type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+
+        soyut.Services.getInstance().getService("browserServer").generateDocxKey({}, function (err, data) {
+            initDocxEditor(data.key, data.vkey);
+
+            function initDocxEditor(docKey, docVkey) {
+                var docEditor = new DocsAPI.DocEditor(frameEditorId[1],
+                    {
+                        width: "800px",
+                        height: "800px",
+                        documentType: "text",
+                        document: {
+                            title: name,
+                            url: url,
+                            key: docKey,
+                            vkey: docVkey,
+                            permissions: {
+                                download: false,
+                                print: false,
+                            }
+                        },
+                        editorConfig: {
+                            lang: "en",
+                            location: documentServerUrl + "/web-apps/",
+                            customization: {
+                                about: false,
+                                logo: {
+                                    image: 'https://' + browserService.origin + '/img/soyut.png'
+                                }
+                            }
+                        }
+                    });
+            }
+        });
+
+    }
+    else if (type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") {
+        soyut.Services.getInstance().getService("browserServer").generateSheetxKey({}, function (err, data) {
+            initSheetxEditor(data.key, data.vkey);
+            function initSheetxEditor(docKey, docVkey) {
+                var docEditor = new DocsAPI.DocEditor(frameEditorId[1],
+                    {
+                        width: "800px",
+                        height: "800px",
+                        documentType: "spreadsheet",
+                        document: {
+                            title: name,
+                            url: url,
+                            key: docKey,
+                            vkey: docVkey,
+                            permissions: {
+                                download: false,
+                                print: false,
+                            }
+                        },
+                        editorConfig: {
+                            lang: "en",
+                            location: documentServerUrl + "/web-apps/",
+                            customization: {
+                                about: false,
+                                logo: {
+                                    image: 'https://' + browserService.origin + '/img/soyut.png'
+                                }
+                            }
+                        }
+                    });
+            }
+        });
+
+    }
+    else if (type == "application/vnd.openxmlformats-officedocument.presentationml.presentation") {
+        soyut.Services.getInstance().getService("browserServer").generatePresentationxKey({}, function (err, data) {
+            initPresentationxEditor(data.key, data.vkey);
+            function initPresentationxEditor(docKey, docVkey) {
                 var docEditor = new DocsAPI.DocEditor(frameEditorId[1],
                     {
                         width: "800px",
